95b4eda15935235c1961a5f78b145247a37956a5
Alex Leith <alexgleith+github@gmail.com>
2013-08-09 16:06:54 +1000

    working php

--- src/autocomplete.php
+++ src/autocomplete.php
@@ -1,9 +1,8 @@
 <?php
  
 // if the 'term' variable is not sent with the request, exit
-//	exit;
-// 
-echo 'ran';
+if ( !isset($_REQUEST['term']) )
+	exit;
 // connect to the database server and select the appropriate database for use
 $conn = pg_pconnect("host=localhost port=5432 dbname=GISDB user=nginx password=phptest");
 if (!$conn) {
@@ -11,23 +10,33 @@ if (!$conn) {
   exit;
 }
 
-$query = trim('12 main ');
+$query = trim($_REQUEST['term']);
 $query_array = explode(' ', $query);
- 
-// loop through each zipcode returned and format the response for jQuery
-try {	
-	$house_no = "%" . $query_array[0] . "%";
-	$address = "%" . $query . "%";
-	$params = array($house_no, $address);
-	echo $params;
-	$result = pg_query_params($conn,'SELECT pid, concat(addr_line2,\', \',addr_line3) as address, geom FROM "Property" where addr_line2 like $1 and house_no like $2 LIMIT 10',array($house_no,$address));
-	//$result = pg_query($conn, 'SELECT pid, concat(addr_line2,\', \',addr_line3) as address, geom FROM "Property" where addr_line2 like \'%12 Main%\' and house_no like \'%12%\' LIMIT 10');
 
-	$results = pg_fetch_all($result)
-	$json = json_encode($results);
-	echo $json
-}
+$house_no = "%" . $query_array[0] . "%";
+$address = "%" . $query . "%";
+$sql = "SELECT pid, concat(addr_line2,', ',addr_line3) as address, geom FROM \"Property\" where addr_line2 like '{$address}' and house_no like '{$house_no}' LIMIT 10";
+
+$result = pg_query($conn, $sql);
+
+$results = pg_fetch_all($result)
+$json = json_encode($results);
+echo $json;
  
-// jQuery wants JSON data
-echo json_encode($data);
 flush();
+/*
+if($result !== FALSE)
+{
+  $row = @pg_num_rows($result);
+echo $row;
+  if ( $row !== 0 )
+         echo "hurray: " . $row;
+  else
+         echo "argh";
+}
+else
+{
+  echo "Seems, your query is bad.";
+}
+*/
+?>
