a6c388374a891d4fc0c9972ef4ff5c3a07097201
Alex Leith <alexgleith+github@gmail.com>
2014-02-05 12:39:36 +1100

    changed list URl to https

--- src/listBaseLayers.js
+++ src/listBaseLayers.js
@@ -1,11 +1,11 @@
 //Define layers
-var LISTTopographic = new L.tileLayer("http://services.thelist.tas.gov.au/arcgis/rest/services/Basemaps/Topographic/ImageServer/tile/{z}/{y}/{x}", {
+var LISTTopographic = new L.tileLayer("https://services.thelist.tas.gov.au/arcgis/rest/services/Basemaps/Topographic/ImageServer/tile/{z}/{y}/{x}", {
     attribution: "Basemap &copy The LIST",
     maxZoom: 20,
     maxNativeZoom: 18
 });
 
-var LISTAerial = new L.tileLayer("http://services.thelist.tas.gov.au/arcgis/rest/services/Basemaps/Orthophoto/ImageServer/tile/{z}/{y}/{x}", {
+var LISTAerial = new L.tileLayer("https://services.thelist.tas.gov.au/arcgis/rest/services/Basemaps/Orthophoto/ImageServer/tile/{z}/{y}/{x}", {
     attribution: "Basemap &copy The LIST",
     maxZoom: 20,
     maxNativeZoom: 18
