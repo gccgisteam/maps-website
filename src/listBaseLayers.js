67309937e8cf9396c074fe7597822771a5e36e58
Alex Leith <alexgleith+github@gmail.com>
2014-03-17 08:38:48 +1100

    changed native zoom for list basemaps

--- src/listBaseLayers.js
+++ src/listBaseLayers.js
@@ -8,5 +8,5 @@ var LISTTopographic = new L.tileLayer("https://services.thelist.tas.gov.au/arcgi
 var LISTAerial = new L.tileLayer("https://services.thelist.tas.gov.au/arcgis/rest/services/Basemaps/Orthophoto/ImageServer/tile/{z}/{y}/{x}", {
     attribution: "Basemap &copy The LIST",
     maxZoom: 20,
-    maxNativeZoom: 18
+    maxNativeZoom: 19
 });
