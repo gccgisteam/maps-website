0e41680861f647b413c380229dfa499fe6b5ee38
Alex Leith <alexgleith@gmail.com>
2015-01-12 16:36:30 +1100

    updated attribution

--- src/listBaseLayers.js
+++ src/listBaseLayers.js
@@ -1,12 +1,12 @@
 //Define layers
 var LISTTopographic = new L.tileLayer("https://services.thelist.tas.gov.au/arcgis/rest/services/Basemaps/Topographic/ImageServer/tile/{z}/{y}/{x}", {
-    attribution: "Basemap &copy The LIST",
+    attribution: "Topographic Basemap from <a href=http://www.thelist.tas.gov.au>the LIST</a> &copy State of Tasmania",
     maxZoom: 20,
     maxNativeZoom: 18
 });
 
 var LISTAerial = new L.tileLayer("https://services.thelist.tas.gov.au/arcgis/rest/services/Basemaps/Orthophoto/ImageServer/tile/{z}/{y}/{x}", {
-    attribution: "Basemap &copy The LIST",
+    attribution: "Base Imagery from <a href=http://www.thelist.tas.gov.au>the LIST</a> &copy State of Tasmania",
     maxZoom: 20,
     maxNativeZoom: 19
 });
